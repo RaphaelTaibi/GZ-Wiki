30e2a4dd87c9e8b508c37e1717571854b80bddf3
Taibi Raphael <83208861+RaphaelTaibi@users.noreply.github.com>
2024-08-20 09:07:24 +0200

    Update Home.js (deletion old import)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-//import backgroundImage from "../../assets/images/GrayZone.webp";
-//import styles from "./Home.module.scss";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
